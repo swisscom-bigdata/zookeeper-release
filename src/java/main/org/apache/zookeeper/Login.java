bcc7fd78602a3e9a296cc54f7bed22cf8c312dba
Sergey Shelukhin <sershe@apache.org>
2016-07-13 17:19:13 -0700

    BUG-61839 : temporary logging before ZOOKEEPER-2469 is committer (Sergey Shelukhin, reviewed by Enis Soztutar) ADDENDUM

--- src/java/main/org/apache/zookeeper/Login.java
+++ src/java/main/org/apache/zookeeper/Login.java
@@ -232,7 +232,7 @@ public class Login {
                             } catch (LoginException le) {
                                 if (retry > 0) {
                                     --retry;
-                                    LOG.warn("Could not refresh TGT for principal: {}. Will retry.", principal, le);
+                                    LOG.warn("Could not refresh TGT for principal: " + principal + ". Will retry.", le);
                                     // sleep for 10 seconds.
                                     try {
                                         Thread.sleep(10 * 1000);
